c3f95d67843a1503820ae1946942e2ac638dc3d5
Jan <86538212+Vanbastion@users.noreply.github.com>
2022-08-23 17:15:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonmezer_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jan Sonmezer! Nice day for fishing ain't it?");
+        }
+    }
+}
